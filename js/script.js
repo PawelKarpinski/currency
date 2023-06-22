5ff7b6dfe7bab39a3dcd6e5638be05ea8dc5e97f
Paweł Karpiński <pawel.karpinski7@outlook.com>
2023-06-22 22:14:37 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let formElement = document.querySelector(".js-form");
+let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
+let resultEemenet = document.querySelector(".js-result");
+
+let rateEUR = 4.4408;
+let rateGBP = 5.1703;
+let rateUSD = 4.0648;
+
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
+
+    let result;
+
+    switch (currency) {
+     case "EUR":
+        result = amount / rateEUR;
+        break;
+    case "GBP":
+        result = amount / rateGBP
+        break;
+    case "USD":
+        result = amount / rateUSD;
+        break;
+    }
+    resultEemenet.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong> `
+ });
